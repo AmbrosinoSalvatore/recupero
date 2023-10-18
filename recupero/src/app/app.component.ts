89698bcbc8933588e011c3d05a340143423146e4
sasyvelobelo <ambrosino.salvatore@iisgalvanimi.edu.it>
2023-10-18 10:55:17 +0000

    gg

---
+++ recupero/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TypeList } from './Typelist.module';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'recupero';
+  pokemonList! : TypeList  ;
+  obs! :Observable <TypeList>;
+  constructor(private http :HttpClient)
+  {
+    this.obs = this.http.get<TypeList>("https://pokeapi.co/api/v2/type");
+    this.obs.subscribe(this.doSomething)
+  }
+  doSomething = (data: TypeList) => {
+    this.pokemonList = data
+  }
+}
